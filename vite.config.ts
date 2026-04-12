a10ab76f7fbfa6f0f1e6d8cb5c1009af49d346f8
Chris Earl Amar <chrisearlamar@gmail.com>
2026-04-13 07:37:00 +0800

    CI - Adjusted Vite Build Config, Renamed CSS FIle

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,23 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Name the main JavaScript file
+        entryFileNames: 'assets/script.js',
+
+        // Name dynamically imported JS chunks
+        chunkFileNames: 'assets/script.js',
+
+        // Name CSS and other asset files
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/style.css';
+          }
+          return 'assets/[name][extname]';
+        },
+      },
+    },
+  },
 })
